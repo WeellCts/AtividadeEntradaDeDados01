606b4183d066eac2cf4ce27e94e0dfd78240b0e1
WeellCts <wellingtonmccosta67@gmail.com>
2026-09-03 00:18:17 +0000

    exercicio-01-cadastro-simples

--- exercicio-01-cadastro-simples/src/App.java
+++ exercicio-01-cadastro-simples/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+
+        // ENTRADA
+        System.out.print("Digite o nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.print("Digite a idade: ");
+        int idade = entrada.nextInt();
+        entrada.nextLine(); // Limpa o buffer do teclado
+
+        System.out.print("Digite a cidade: ");
+        String cidade = entrada.nextLine();
+
+        // PROCESSAMENTO E SAÍDA
+        System.out.println("\n--- RESUMO DO CADASTRO ---");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Cidade: " + cidade);
+
+        entrada.close();
     }
 }
